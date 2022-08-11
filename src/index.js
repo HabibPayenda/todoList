364bce4dd15878b8def6151f26fb227191b901ab
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-08-11 10:40:28 +0430

    Changed the styles as requested

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './style.css';
 import {
   tasks,
   addTask,
-  removeTask,
   editTask,
 } from './task.js';
 
@@ -10,19 +9,14 @@ const list = document.getElementById('list');
 const refresh = document.getElementById('refresh');
 const addIcon = document.getElementById('addIcon');
 const text = document.getElementById('text');
-const message = document.getElementById('message');
-const messageText = document.getElementById('messageText');
 
 refresh.addEventListener('click', () => {
   document.location.reload();
-  messageText.innerText = '';
 });
 
 addIcon.addEventListener('click', () => {
   const desc = text.value;
   addTask(desc);
-  message.style.display = 'flex';
-  messageText.innerText = 'Task added, Please refresh!';
 });
 
 tasks.forEach((task) => {
@@ -38,47 +32,14 @@ tasks.forEach((task) => {
   todo.classList.add('todo');
   todo.innerText = task.desc;
 
-  const menu = document.createElement('div');
-  menu.classList.add('menu');
-
-  const menuClose = document.createElement('i');
-  menuClose.classList.add('fas');
-  menuClose.classList.add('fa-window-close');
-  menuClose.classList.add('menuClose');
-  menuClose.addEventListener('click', () => {
-    menu.style.display = 'none';
-  });
-
-  const deleteItem = document.createElement('div');
-  deleteItem.classList.add('menuItem');
-  deleteItem.classList.add('deleteItem');
-  deleteItem.innerText = 'Remove';
-  deleteItem.addEventListener('click', () => {
-    menu.style.display = 'none';
-    removeTask(task.index);
-    message.style.display = 'flex';
-    message.style.backgroundColor = 'red';
-    messageText.innerText = 'Task removed, Please refresh!';
-  });
-
-  const editItem = document.createElement('div');
-  editItem.classList.add('menuItem');
-  editItem.classList.add('editItem');
-  editItem.innerText = 'Edit';
-  editItem.addEventListener('click', () => {
-    editTask(task.index);
-  });
-
-  menu.append(menuClose, deleteItem, editItem);
-
   const listItemIcon = document.createElement('i');
   listItemIcon.classList.add('listItemIcon');
   listItemIcon.classList.add('fas');
   listItemIcon.classList.add('fa-ellipsis-v');
   listItemIcon.addEventListener('click', () => {
-    menu.style.display = 'flex';
+    editTask(task.index);
   });
 
-  listItem.append(check, todo, listItemIcon, menu);
+  listItem.append(check, todo, listItemIcon);
   list.append(listItem);
 });
--- src/task.js
+++ src/task.js
@@ -12,37 +12,49 @@ export const addTask = (desc) => {
   const task = new Task(desc, tasks.length + 1);
   const newTasks = [...tasks, task];
   localStorage.setItem('tasks', JSON.stringify(newTasks));
+  document.location.reload();
 };
 
-export const removeTask = (index) => {
+const removeTask = (index) => {
   let newTasks = tasks.filter((task) => task.index !== index);
   newTasks = newTasks.map((task, index) => {
     task.index = index + 1;
     return task;
   });
   localStorage.setItem('tasks', JSON.stringify(newTasks));
+  document.location.reload();
+};
+
+const updateTask = (index, editInput) => {
+  const newTasks = tasks.filter((item) => item.index !== index);
+
+  const newTask = new Task(editInput.value, index);
+  newTasks.splice(index - 1, 0, { ...newTask });
+
+  localStorage.setItem('tasks', JSON.stringify(newTasks));
+  document.location.reload();
 };
 
 export const editTask = (index) => {
   const task = tasks[index - 1];
   const item = document.getElementById(index);
+  item.style.backgroundColor = '#d4d46e';
   item.innerHTML = '';
   const editInput = document.createElement('input');
   editInput.classList.add('editInput');
   editInput.value = task.desc;
+  editInput.style.backgroundColor = '#d4d46e';
+
+  editInput.addEventListener('focusout', () => {
+    updateTask(index, editInput);
+  });
 
   const menuOk = document.createElement('i');
   menuOk.classList.add('fas');
-  menuOk.classList.add('fa-check');
+  menuOk.classList.add('fa-trash');
   menuOk.classList.add('menuOk');
   menuOk.addEventListener('click', () => {
-    const newTasks = tasks.filter((item) => item.index !== index);
-
-    const newTask = new Task(editInput.value, index);
-    newTasks.splice(index - 1, 0, { ...newTask });
-
-    localStorage.setItem('tasks', JSON.stringify(newTasks));
-    document.location.reload();
+    removeTask(index);
   });
 
   item.append(editInput, menuOk);
