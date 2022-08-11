d1fd5e0339ce55019e89f1e612de7000aac8c68e
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-08-11 17:17:18 +0430

    ClearAllCompleted implemented

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import './style.css';
 import {
-  tasks,
   addTask,
   editTask,
+  removeAllCompleted,
 } from './task.js';
+import changeStatus from './status.js';
 
 const list = document.getElementById('list');
 const refresh = document.getElementById('refresh');
 const addIcon = document.getElementById('addIcon');
 const text = document.getElementById('text');
+const removeAll = document.getElementById('removeAll');
+const tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : [];
 
 refresh.addEventListener('click', () => {
   document.location.reload();
@@ -19,6 +22,10 @@ addIcon.addEventListener('click', () => {
   addTask(desc);
 });
 
+removeAll.addEventListener('click', () => {
+  removeAllCompleted();
+});
+
 tasks.forEach((task) => {
   const listItem = document.createElement('li');
   listItem.classList.add('listItem');
@@ -27,10 +34,18 @@ tasks.forEach((task) => {
   const check = document.createElement('input');
   check.setAttribute('type', 'checkbox');
   check.classList.add('check');
+  check.addEventListener('change', () => {
+    changeStatus(task.index);
+  });
 
   const todo = document.createElement('p');
   todo.classList.add('todo');
   todo.innerText = task.desc;
+  if (task.completed) {
+    todo.style.textDecoration = 'line-through';
+    todo.style.color = '#999';
+    check.checked = true;
+  }
 
   const listItemIcon = document.createElement('i');
   listItemIcon.classList.add('listItemIcon');
