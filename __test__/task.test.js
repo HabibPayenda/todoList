6bc895eebc2ed77f7c357b49729be4649d041605
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-08-19 12:42:55 +0430

    The funcitons for tasks added and tested the taskCompleted and removeAllCompleted functions

--- __test__/task.test.js
+++ __test__/task.test.js
@@ -1,4 +1,14 @@
-import { addTask, removeTask, localStorage } from '../__mocks__/functions.js';
+/**
+ * @jest-environment jsdom
+ */
+
+import {
+  addTask,
+  removeTask,
+  localStorage,
+  setCompleted,
+  removeAllCompleted,
+} from '../__mocks__/functions.js';
 
 describe('Adding a task to the list', () => {
   it('should add a task to the list', () => {
@@ -15,11 +25,29 @@ describe('Adding a task to the list', () => {
   });
 });
 
-describe('', () => {
+describe('Removing a task from the list', () => {
   it('should remove an Item from the task', () => {
     const index = 1;
     removeTask(index);
 
     expect(localStorage.list[0]).toBeUndefined();
+  });
+});
+
+describe('Completed task functions', () => {
+  it('should set the task as completed', () => {
+    const index = 1;
+    const desc = 'new';
+    addTask(desc);
+    setCompleted(index);
+
+    expect(localStorage.list[index - 1].completed).toBeTruthy();
+  });
+
+  it('should remove all the completed tasks', () => {
+    removeAllCompleted();
+    const check = localStorage.list.filter((task) => task.completed === true);
+
+    expect(check.length).toBe(0);
   });
 });
